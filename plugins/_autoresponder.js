28793a06655c0195d0924dc429a79dc7d81fb148
OUSSAMA <escastark@gmail.com>
2024-09-22 20:18:31 +0100

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,22 +1,52 @@
 import axios from 'axios';
-https://t.me/globalgb
+import { sticker } from '../lib/sticker.js';
 
-▶︎ *Whatsapp Channels:*▪️
-- https://whatsapp.com/channel/0029VapZaaTKbYMEBADmfz1v
+let handler = m => m;
 
-If you are asked how to host Gatabot 24/7, we recommend our host "corinPlus host", quality hosting, we offer free and paid plans at affordable prices, cheap that everyone can acquire, we have several payment methods. 
+handler.all = async function (m, {conn}) {
+let chat = global.db.data.chats[m.chat];
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
 
- Important information:
+//if (prefixRegex.test(m.text)) return true;
+if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
+if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
+await this.sendPresenceUpdate('composing', m.chat);
 
-▶︎ *WhatsApp Community:*
+async function luminsesi(q, username, logic) {
+try {
+const response = await axios.post("https://luminai.my.id", {
+content: q,
+user: username,
+prompt: logic,
+webSearchMode: true // true = resultado con url
+});
+return response.data.result;
+} catch (error) {
+console.error(error);
+}}
+
+let query = m.text;
+let username = `${m.pushName}`;
+let syms1 = `
+
+Official GitHub: https://github.com/Unholy-00 
+
+información importante:
+
+▶︎ *WhatsApp community:*
 https://chat.whatsapp.com/Bb5cUhAof6RBEA8S4XSRaK
 
-▶︎ *WhatsApp Channel:*
+▶︎ *WhatsApp channel:*
 https://whatsapp.com/channel/0029VapZaaTKbYMEBADmfz1v
 
-▶︎*Contact:*
+▶︎ *Contacto:*
 • wa.me/212693294821
-• ${ig}
-• https://www.facebook.com/elrebelde21`
+• ${ig}`
 
-let result = await luminsesi(query,  username, syms1) if (m.fromMe) return await m.reply(m.chat, result, m)} return true } export default handler;
+let result = await luminsesi(query, username, syms1)
+if (m.fromMe) return
+await m.reply(m.chat, result, m)}
+return true
+}
+
+export default handler;
