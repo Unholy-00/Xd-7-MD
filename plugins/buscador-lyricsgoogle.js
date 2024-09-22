56ca09563a75e700a7db50d8b1f2f5af40b5baa7
OUSSAMA <escastark@gmail.com>
2024-09-22 19:02:34 +0100

    Update buscador-lyricsgoogle.js

--- plugins/buscador-lyricsgoogle.js
+++ plugins/buscador-lyricsgoogle.js
@@ -7,11 +7,11 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
     let res = await BuscarLetra(text);
     let { titulo, artista, albulm, fecha, Generos, letra } = res;
     let txt = `*𝙂𝙊𝙊𝙂𝙇𝙀 𝙇𝙔𝙍𝙄𝘾𝙎*\n\n`;
-    txt += ` *↬ ${mid.smsYT1}:* ${titulo}\n`;
-    txt += ` *↬ ${mid.smsYT6}:* ${artista}\n`;
+    txt += ` *↬ ${mid.smsYT1}:* ${title}\n`;
+    txt += ` *↬ ${mid.smsYT6}:* ${artist}\n`;
     txt += ` *↬ ${mid.smsYT7}:* ${albulm}\n`;
-    txt += ` *↬ ${mid.smsYT8}:* ${fecha}\n`;
-    txt += ` *↬ ${mid.smsYT8}:* ${Generos}\n\n`;
+    txt += ` *↬ ${mid.smsYT8}:* ${date}\n`;
+    txt += ` *↬ ${mid.smsYT8}:* ${Gene}\n\n`;
     txt += `${letra}`;
 m.reply(txt);
 } catch (e) {
